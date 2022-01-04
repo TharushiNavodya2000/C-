bdd34ff9521aecf9b7980bf827a8ec365b0443e3
Tharushi Navodya <87310863+TharushiNavodya2000@users.noreply.github.com>
2022-01-04 23:08:59 +0000

    Inclass 1 Q

---
+++ Inclass1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int marks1;
+	int marks2;
+
+	cout << "Enter the mark 1 :";
+	cin >> marks1;
+
+	cout << "Enter the mark 2:";
+	cin >> marks2;
+
+	cout << "Avarage of marks :" << (marks1 +marks2 )/ 2.0;
+
+	return 0;
+}
